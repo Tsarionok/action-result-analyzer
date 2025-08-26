408f2178d67b82482e390229d62a7e1997ae75b7
Sergey Tsarionok <tsarionok@gmail.com>
2025-08-26 15:11:48 +0300

    add ActionResult in Task case support

--- ActionResultAnalyzer/Analyzers/ActionResultTypeAnalyzer.cs
+++ ActionResultAnalyzer/Analyzers/ActionResultTypeAnalyzer.cs
@@ -1,101 +1,157 @@
-using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
 
-namespace ActionResultAnalyzer.Analyzers;
-
-[DiagnosticAnalyzer(LanguageNames.CSharp)]
-public class ActionResultTypeAnalyzer : DiagnosticAnalyzer
+namespace ActionResultAnalyzer
 {
-    public const string DiagnosticId = "ACT001";
-    
-    private static readonly DiagnosticDescriptor Rule = new(
-        DiagnosticId,
-        "Invalid return type for ActionResult<T>",
-        "Method returning ActionResult<{0}> cannot return '{1}'",
-        "TypeSafety",
-        DiagnosticSeverity.Error,
-        true,
-        "ActionResult<T> methods should only return T or ActionResult-derived types");
-
-    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics 
-        => [Rule];
-
-    public override void Initialize(AnalysisContext context)
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ActionResultTypeAnalyzer : DiagnosticAnalyzer
     {
-        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
-        context.EnableConcurrentExecution();
+        public const string DiagnosticId = "ACT001";
         
-        context.RegisterSyntaxNodeAction(AnalyzeMethodDeclaration, SyntaxKind.MethodDeclaration);
-    }
+        private static readonly DiagnosticDescriptor Rule = new(
+            DiagnosticId,
+            "Invalid return type for ActionResult<T>",
+            "Method returning ActionResult<{0}> cannot return '{1}'",
+            "TypeSafety",
+            DiagnosticSeverity.Error,
+            true,
+            "ActionResult<T> methods should only return T, ActionResult, Task<T>, Task<ActionResult>, etc.");
 
-    private void AnalyzeMethodDeclaration(SyntaxNodeAnalysisContext context)
-    {
-        if (context.Node is not MethodDeclarationSyntax methodDeclaration)
-            return;
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics 
+            => ImmutableArray.Create(Rule);
 
-        if (methodDeclaration.ReturnType is not GenericNameSyntax { Identifier.Text: "ActionResult" } genericName)
-            return;
-
-        var typeArgument = genericName.TypeArgumentList.Arguments.FirstOrDefault();
-        if (typeArgument is null) return;
-
-        var expectedTypeSymbol = context.SemanticModel.GetTypeInfo(typeArgument).Type;
-        if (expectedTypeSymbol is null) return;
-
-        foreach (var returnStatement in methodDeclaration.DescendantNodes().OfType<ReturnStatementSyntax>())
+        public override void Initialize(AnalysisContext context)
         {
-            if (returnStatement.Expression is null) continue;
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            
+            context.RegisterSyntaxNodeAction(AnalyzeMethodDeclaration, SyntaxKind.MethodDeclaration);
+        }
 
-            var returnedExpressionType = context.SemanticModel.GetTypeInfo(returnStatement.Expression).Type;
-            if (returnedExpressionType is null) continue;
-
-            if (!IsTypeCompatible(returnedExpressionType, expectedTypeSymbol, context))
+        private void AnalyzeMethodDeclaration(SyntaxNodeAnalysisContext context)
+        {
+            var methodDeclaration = (MethodDeclarationSyntax)context.Node;
+            
+            if (methodDeclaration.ReturnType is not GenericNameSyntax genericName ||
+                genericName.Identifier.Text != "ActionResult")
             {
-                context.ReportDiagnostic(Diagnostic.Create(
-                    Rule,
-                    returnStatement.Expression.GetLocation(),
-                    expectedTypeSymbol.Name,
-                    returnedExpressionType.Name));
+                return;
+            }
+            
+            var typeArgument = genericName.TypeArgumentList.Arguments.FirstOrDefault();
+            if (typeArgument == null) return;
+
+            var expectedTypeSymbol = context.SemanticModel.GetTypeInfo(typeArgument).Type;
+            if (expectedTypeSymbol == null) return;
+            
+            var returnStatements = methodDeclaration.DescendantNodes()
+                .OfType<ReturnStatementSyntax>();
+
+            foreach (var returnStatement in returnStatements)
+            {
+                if (returnStatement.Expression == null) continue;
+
+                var returnedExpressionType = context.SemanticModel.GetTypeInfo(returnStatement.Expression).Type;
+                if (returnedExpressionType == null) continue;
+                
+                if (!IsTypeCompatible(returnedExpressionType, expectedTypeSymbol, context))
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(
+                        Rule,
+                        returnStatement.Expression.GetLocation(),
+                        expectedTypeSymbol.Name,
+                        returnedExpressionType.Name));
+                }
             }
         }
-    }
 
-    private static bool IsTypeCompatible(ITypeSymbol returnedType, ITypeSymbol expectedType, SyntaxNodeAnalysisContext context)
-    {
-        if (SymbolEqualityComparer.Default.Equals(returnedType, expectedType))
-            return true;
-
-        var actionResultType = context.Compilation.GetTypeByMetadataName("Microsoft.AspNetCore.Mvc.ActionResult");
-        var actionResultTType = context.Compilation.GetTypeByMetadataName("Microsoft.AspNetCore.Mvc.ActionResult`1");
-        
-        if (actionResultType is not null && returnedType.InheritsFrom(actionResultType))
-            return true;
-
-        if (actionResultTType is not null && 
-            SymbolEqualityComparer.Default.Equals(returnedType.OriginalDefinition, actionResultTType) &&
-            returnedType is INamedTypeSymbol { TypeArguments: [var actualTypeArgument] })
+        private bool IsTypeCompatible(ITypeSymbol returnedType, ITypeSymbol expectedType, SyntaxNodeAnalysisContext context)
         {
-            return SymbolEqualityComparer.Default.Equals(actualTypeArgument, expectedType);
-        }
-
-        return false;
-    }
-}
-
-public static class SymbolExtensions
-{
-    public static bool InheritsFrom(this ITypeSymbol type, ITypeSymbol possibleBaseType)
-    {
-        var current = type;
-        while (current is not null)
-        {
-            if (SymbolEqualityComparer.Default.Equals(current, possibleBaseType))
+            if (SymbolEqualityComparer.Default.Equals(returnedType, expectedType))
                 return true;
-            current = current.BaseType;
+            
+            if (returnedType.IsTaskWrapper(expectedType, context))
+                return true;
+            
+            var actionResultTypeName = "Microsoft.AspNetCore.Mvc.ActionResult";
+            var actionResultTTypeName = "Microsoft.AspNetCore.Mvc.ActionResult`1";
+            
+            if (returnedType.InheritsFrom(actionResultTypeName))
+                return true;
+
+            if (returnedType.IsGenericActionResult(actionResultTTypeName, expectedType))
+                return true;
+
+            return false;
         }
-        return false;
+    }
+
+    public static class SymbolExtensions
+    {
+        public static bool InheritsFrom(this ITypeSymbol type, string fullTypeName)
+        {
+            var current = type;
+            while (current is not null)
+            {
+                if (current.ToDisplayString() == fullTypeName)
+                    return true;
+                current = current.BaseType;
+            }
+            return false;
+        }
+
+        public static bool IsGenericActionResult(this ITypeSymbol type, string actionResultTTypeName, ITypeSymbol expectedType)
+        {
+            if (type is not INamedTypeSymbol namedType || !namedType.IsGenericType)
+                return false;
+
+            var originalDefinition = namedType.OriginalDefinition;
+            if (originalDefinition.ToDisplayString() != actionResultTTypeName)
+                return false;
+
+            return namedType.TypeArguments.Length == 1 && 
+                   SymbolEqualityComparer.Default.Equals(namedType.TypeArguments[0], expectedType);
+        }
+
+        public static bool IsTaskWrapper(this ITypeSymbol type, ITypeSymbol expectedType, SyntaxNodeAnalysisContext context)
+        {
+            if (type is INamedTypeSymbol namedType && namedType.IsGenericType)
+            {
+                var originalDefinition = namedType.OriginalDefinition;
+                var originalDefinitionName = originalDefinition.ToDisplayString();
+                
+                if (originalDefinitionName is "System.Threading.Tasks.Task<T>" or "System.Threading.Tasks.ValueTask<T>")
+                {
+                    if (namedType.TypeArguments.Length == 1)
+                    {
+                        var taskResultType = namedType.TypeArguments[0];
+                        
+                        return IsTypeCompatible(taskResultType, expectedType, context);
+                    }
+                }
+            }
+            
+            return false;
+        }
+
+        private static bool IsTypeCompatible(ITypeSymbol returnedType, ITypeSymbol expectedType, SyntaxNodeAnalysisContext context)
+        {
+            var actionResultTypeName = "Microsoft.AspNetCore.Mvc.ActionResult";
+            var actionResultTTypeName = "Microsoft.AspNetCore.Mvc.ActionResult`1";
+            
+            if (SymbolEqualityComparer.Default.Equals(returnedType, expectedType))
+                return true;
+
+            if (returnedType.InheritsFrom(actionResultTypeName))
+                return true;
+
+            if (returnedType.IsGenericActionResult(actionResultTTypeName, expectedType))
+                return true;
+
+            return false;
+        }
     }
 }
